182fbf432739484a26f39ec2a9b775f381d0e2cb
Camila Vargas Mejia <cavargasme@unal.edu.co>
2025-05-24 08:10:22 -0500

    e1

---
+++ homework/__main__.py
@@ -0,0 +1,9 @@
+"""Entry point for the homework package."""
+
+# python3 -m homework data/input data/output
+# para correr en consola: python -m homework --model knn
+
+from .src.main import main
+
+if __name__ == "__main__":
+    main()
